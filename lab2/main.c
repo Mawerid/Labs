5f6c919fac3124aca0f8ba93de496cb62c386cdc
Mawerid <monasmax02@gmail.com>
2021-10-02 15:02:48 +0300

    new version

---
+++ lab2/main.c
@@ -0,0 +1,14 @@
+#include "cryptaes.h"
+
+int main() {
+    FILE *in;
+    in = fopen("input.txt", "r");
+    unsigned key[KEYCOUNT] = {0xabcdef71, 0, 0};
+    generate_keys(key[0],&key[1],&key[2]);
+    unsigned cipher;
+    cipher = cbc(key, 0x01234567, 0xb6a4ea1b, 1);
+    printf("%x", cipher);
+    cipher = cbc(key, 0x89abcdef, cipher, 1);
+    printf("%x", cipher);
+    return 0;
+}
