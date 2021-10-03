00ce20cf1198265b76912c86927d178e13244f3c
Mawerid <monasmax02@gmail.com>
2021-10-03 22:26:42 +0300

    Add file reading

--- lab2/main.c
+++ lab2/main.c
@@ -2,15 +2,71 @@
 
 #define KEYCOUNT 3
 
+unsigned str_hex(const char* str);
+
 int main() {
+    char * filename = "input.txt";
+    unsigned init_vector = 0xb6a4ea1b;
+    unsigned key0 = 0xabcdef71;
+    int enc = 1;
+    int typeisecb = 1;
+
     FILE *in;
-    in = fopen("input.txt", "r");
-    unsigned key[KEYCOUNT] = {0xabcdef71, 0, 0};
-    generate_keys(key[0],&key[1],&key[2]);
+    in = fopen(filename, "r");
+    char c;
+    char hex_block[] = "00000000";
+    int i = 0;
+    unsigned inf_block;
+    unsigned key[KEYCOUNT] = {key0, 0, 0};
     unsigned cipher;
-    cipher = cbc(key, 0x01234567, 0xb6a4ea1b, 1);
-    printf("%x", cipher);
-    cipher = cbc(key, 0x89abcdef, cipher, 1);
-    printf("%x", cipher);
+
+    generate_keys(key[0],&key[1],&key[2]);
+
+    while ((c = fgetc(in)) != EOF) {
+        hex_block[i] = c;
+        i++;
+        if (i == 8) {
+            inf_block = str_hex(hex_block);
+            if (typeisecb) {
+                cipher = ecb(key, inf_block, enc);
+                printf("%x", cipher);
+            } else {
+                cipher = cbc(key, inf_block, init_vector, enc);
+                init_vector = cipher;
+                printf("%x", cipher);
+            }
+            i = 0;
+            for (int j = 0; j < 8; j++)
+                hex_block[j] = '0';
+        }
+    }
+    if (i != 0) {
+        inf_block = str_hex(hex_block);
+        if (typeisecb) {
+            cipher = ecb(key, inf_block, enc);
+            printf("%x", cipher);
+        } else {
+            cipher = cbc(key, inf_block, init_vector, enc);
+            init_vector = cipher;
+            printf("%x", cipher);
+        }
+    }
+    printf("\n");
     return 0;
 }
+
+unsigned str_hex(const char * str){
+    const char * ptr;
+    unsigned num;
+    num = 0;
+    for (ptr = str; *ptr; ptr++) {
+        if (*ptr >= '0' && *ptr <= '9')
+            num = (num << 4) | (unsigned int)(*ptr - '0');
+        else if (*ptr >= 'A' && *ptr <= 'F')
+            num = (num << 4) | (unsigned int)(*ptr - 'A' + 10);
+        else if (*ptr >= 'a' && *ptr <= 'f')
+            num = (num << 4) | (unsigned int)(*ptr - 'a' + 10);
+    }
+    return num;
+}
+
